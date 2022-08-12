7925aededbb0147f8f6e0720c82cd42aa592e4fe
Kavindu handapangoda <kavinduhandapangoda@gmail.com>
2022-08-13 00:46:09 +0530

    first commit

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -12,27 +12,27 @@ export class DataService {
   constructor(private httpClient:HttpClient,public _AESEncryptDecryptService: AESEncryptDecryptService) { }
 
   getData(){
-     return this.httpClient.get('http://nicsystem.test/index');
+     return this.httpClient.get('https://nicsystem.test/index');
    }
 
    getDataLevel2(){
-    return this.httpClient.get('http://nicsystem.test/stage2index');
+    return this.httpClient.get('https://nicsystem.test/stage2index');
   }
 
   getDataLevel3(){
-    return this.httpClient.get('http://nicsystem.test/stage3index');
+    return this.httpClient.get('https://nicsystem.test/stage3index');
   }
 
   getPoliceReport(data){
-    return this.httpClient.get('http://nicsystem.test/getPoliceReport/'+data);
+    return this.httpClient.get('https://nicsystem.test/getPoliceReport/'+data);
   }
 
   getPeaceReport(data){
-    return this.httpClient.get('http://nicsystem.test/getPeaceReport/'+data);
+    return this.httpClient.get('https://nicsystem.test/getPeaceReport/'+data);
   }
 
   getPhoto(data){
-    return this.httpClient.get('http://nicsystem.test/getPhoto/'+data);
+    return this.httpClient.get('https://nicsystem.test/getPhoto/'+data);
   }
  
    insertData(data){
@@ -51,65 +51,65 @@ export class DataService {
      .set('content-type', 'application/json')
      .set('Access-Control-Allow-Origin', '*');
     
-     return this.httpClient.post('http://nicsystem.test/create', data, { 'headers': headers });
+     return this.httpClient.post('https://nicsystem.test/create', data, { 'headers': headers });
    }
  
    loginUser(data){
      const headers= new HttpHeaders()
-     return this.httpClient.post('http://nicsystem.test/login', data, { 'headers': headers });
+     return this.httpClient.post('https://nicsystem.test/login', data, { 'headers': headers });
    }
 
    logOut(data){
     const headers= new HttpHeaders()
-    return this.httpClient.post('http://nicsystem.test/logout', data, { 'headers': headers });
+    return this.httpClient.post('https://nicsystem.test/logout', data, { 'headers': headers });
   }
 
    registerUser(data){
     const headers= new HttpHeaders()
-    return this.httpClient.post('http://nicsystem.test/register', data, { 'headers': headers });
+    return this.httpClient.post('https://nicsystem.test/register', data, { 'headers': headers });
   }
 
    updateRequest(data){
     const headers= new HttpHeaders()
-    return this.httpClient.post('http://nicsystem.test/edit', data, { 'headers': headers });
+    return this.httpClient.post('https://nicsystem.test/edit', data, { 'headers': headers });
   }
 
   updateRequestError(data){
     const headers= new HttpHeaders()
-    return this.httpClient.post('http://nicsystem.test/markError', data, { 'headers': headers });
+    return this.httpClient.post('https://nicsystem.test/markError', data, { 'headers': headers });
   }
 
   updateRequestl2(data){
     const headers= new HttpHeaders()
     data.emailsent = this._AESEncryptDecryptService.decrypt(data.email);
-    return this.httpClient.post('http://nicsystem.test/editl2', data, { 'headers': headers });
+    return this.httpClient.post('https://nicsystem.test/editl2', data, { 'headers': headers });
   }
 
   updateRequestl3(data){
     const headers= new HttpHeaders()
     data.emailsent = this._AESEncryptDecryptService.decrypt(data.email);
-    return this.httpClient.post('http://nicsystem.test/editl3', data, { 'headers': headers });
+    return this.httpClient.post('https://nicsystem.test/editl3', data, { 'headers': headers });
   }
 
   searchRequestFromMail(data){
     const headers= new HttpHeaders()
-    return this.httpClient.get('http://nicsystem.test/searchRequestFromMail/'+data, { 'headers': headers });
+    return this.httpClient.get('https://nicsystem.test/searchRequestFromMail/'+data, { 'headers': headers });
   }
 
    getUser(){
     const headers= new HttpHeaders()
-    return this.httpClient.get('http://nicsystem.test/getuser', { 'headers': headers });
+    return this.httpClient.get('https://nicsystem.test/getuser', { 'headers': headers });
   }
 
   uploadImage(data){
     const headersData = new HttpHeaders({'Content-Type':'multipart/form-data; charset=utf-8'});
     console.log(headersData);
-    return this.httpClient.post('http://nicsystem.test/upload',data, {headers: headersData});
+    return this.httpClient.post('https://nicsystem.test/upload',data, {headers: headersData});
   }
 
   generatebarCode(data){
     const headers= new HttpHeaders()
-    return this.httpClient.get('http://nicsystem.test/generatebarcode/'+data, { 'headers': headers });
+    return this.httpClient.get('https://nicsystem.test/generatebarcode/'+data, { 'headers': headers });
   }
 
 }
